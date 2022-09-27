f49453551da6cc9bcbd6366fed404b490d27f4d7
Millie Nakiganda <millienakiganda@gmail.com>
2022-09-27 15:06:40 +0300

    ch: Add Notes View

--- flutternotes/lib/main.dart
+++ flutternotes/lib/main.dart
@@ -45,20 +45,38 @@ class Homepage extends StatelessWidget {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             final user = FirebaseAuth.instance.currentUser;
+
             if (user != null) {
-              if (user.emailVerified == true) {
-                print('verified');
+              if (user.emailVerified) {
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-            return const Text('done');
+
           default:
             return const CircularProgressIndicator();
         }
       },
+    );
+  }
+}
+
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main UI')),
+      body: const Text('hello World'),
     );
   }
 }
